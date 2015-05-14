223d21cd7216ba3ffbfd812feefb5ee4f2b337e6
Jeff Axelrod <junk@theaxelrods.com>
2015-05-14 13:43:12 -0500

    Update scraper.py
    Adding price history

--- scrapezillow/scraper.py
+++ scrapezillow/scraper.py
@@ -117,6 +117,38 @@ def validate_scraper_input(url, zpid):
         )
     return url or urljoin(constants.ZILLOW_URL, "homedetails/{}_zpid".format(zpid))
 
+def _get_ajax_url(soup, label):
+    javascripts = soup.findAll("script",type="text/javascript")
+    for javascript in javascripts:
+	     if javascript.string and javascript.string.find(label):
+	         pattern = r'ajaxURL:"([\w\s/~&=\-\?\^\+\.]*)",jsModule:"' + label +'"'
+                 url = re.search(pattern,'divId:"' + javascript.string)
+                 if url:
+					      url2 = "http://www.zillow.com" + url.group(1)
+					      print url2
+    return url2
+
+def _get_price_history(ajax_url, request_timeout):
+    html = get_raw_html(ajax_url, request_timeout)
+    pattern = r' { "html": "(.*)" }'
+    html = re.search(pattern, html).group(1)
+    html = re.sub(r'\\"',r'"',html) # Correct escaped quotes
+    html = re.sub(r'\\/',r'/',html) # Correct escaped forward slashes
+    soup = BeautifulSoup(html)
+    data = []
+    table = soup.find('table')
+    table_body = table.find('tbody')
+
+    rows = table_body.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele for ele in cols]
+        date = cols[0].get_text()
+        event = cols[1].get_text()
+        price = cols[2].find('span').get_text()
+
+        data.append([date, event, price])
+    return data
 
 def scrape_url(url, zpid, request_timeout):
     """
@@ -130,4 +162,6 @@ def scrape_url(url, zpid, request_timeout):
     results.update(**facts)
     results.update(**_get_sale_info(soup))
     results["description"] = _get_description(soup)
+    ajax_url = _get_ajax_url(soup,"z-hdp-price-history")
+    results["price_history"] = _get_price_history(ajax_url, request_timeout)
     return results
